ac7eac1412a9e1899d6485c1d495887929585b4c
YeetBrain <hcyarla@gmail.com>
2024-06-26 00:07:20 -0500

    justCheck

--- home.php
+++ home.php
@@ -22,7 +22,7 @@ $user_data = check_login($con);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>GPA Calculator</title>
   
-  <!-- CSS for styling the page -->
+  <!-- CSS for styling the page hi-->
   <style>
     /* General styles for the body and html, setting font and background color */
     body, html {
